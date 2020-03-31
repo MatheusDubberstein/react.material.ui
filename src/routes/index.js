68f63b9f0416bb94ea7b722ecf6b8ea5c8622fa1
matheusDubberstein <matheusdub.s@gmail.com>
2020-03-31 11:27:21 -0300

    add: frist commit

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import SignIn from '../pages/SignIn';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/home" component={Home} />
+    </Switch>
+  );
+}
